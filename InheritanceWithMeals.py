0f9d75590451621569810b1cbef9d871445e1de0
Tony Chacon <tchacon87@gmail.com>
2017-11-28 08:03:07 -0500

    Create InheritanceWithMeals.py

---
+++ InheritanceWithMeals.py
@@ -0,0 +1,44 @@
+# Use inheritance to create Derived Classes: Breakfast, Lunch, and Dinner from the Base Class: Meal
+
+class Meal(object):
+
+    def __init__(self):
+        print 'This is a meal'
+
+    def timeOfDay(self):
+        print 'Any time of day'
+
+class Breakfast(Meal):
+    def __init__(self):
+        Meal.__init__(self)
+        print 'This is breakfast'
+
+    def timeOfDay(self):
+        print 'Morning'
+
+class Lunch(Meal):
+
+    def __init__(self):
+        Meal.__init__(self)
+        print 'This is lunch'
+
+    def timeOfDay(self):
+        print 'Afternoon'
+
+
+class Dinner(Meal):
+    def __init__(self):
+        Meal.__init__(self)
+        print 'This is dinner'
+
+    def timeOfDay(self):
+        print 'Evening'
+
+
+b = Breakfast()
+l = Lunch()
+d = Dinner()
+
+print b.timeOfDay()
+print l.timeOfDay()
+print d.timeOfDay()
