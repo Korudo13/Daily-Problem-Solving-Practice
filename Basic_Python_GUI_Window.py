3041a1cf2b2d2b9a67e2b2ec2f2870d5d450d152
Tony Chacon <tchacon87@gmail.com>
2018-01-09 07:31:52 -0500

    Create Basic_Python_GUI_Window.py

---
+++ Basic_Python_GUI_Window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Title for this window")
+
+w = Label(root, text = "Tkinter is one of Python's tools for creating GUIs!")
+w.pack()     
+
+root.mainloop()
